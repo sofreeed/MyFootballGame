186e4c02a8f046173f7c576814bb47e9f015c975
huzhibo <897002622@qq.com>
2025-12-01 01:02:19 +0800

    提交luban3

---
+++ Client/Assets/Scripts/GameMain.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using SimpleJSON;
+using UnityEngine;
+
+public class GameMain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var tables = new cfg.Tables(LoadByteBuf);
+        //UnityEngine.Debug.LogFormat("item[1].name:{0}", tables.TbItem[1].Name);
+
+
+        UnityEngine.Debug.Log("== load succ==");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private static JSONNode LoadByteBuf(string file)
+    {
+        return JSON.Parse(File.ReadAllText(Application.dataPath + "/../../GenerateDatas/json/" + file + ".json", System.Text.Encoding.UTF8));
+    }
+}
